816406d29507dd64d29827369eea2e6613fc2fc5
Aiden Allred <learntocode70@gmail.com>
2024-04-15 14:34:58 -0500

    Fully remove venv

--- main.py
+++ main.py
@@ -2,17 +2,16 @@
 # This is the main file for the project.
 
 ### Imports
-import tsapp
 
+import tsapp
 
 
 def main():
     window = tsapp.GraphicsWindow()
-    
+
     while window.is_running:
         window.finish_frame()
 
 
 if __name__ == "__main__":
     main()
-
